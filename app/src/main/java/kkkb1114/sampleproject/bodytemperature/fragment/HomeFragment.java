7d89fe836c58dffe08b08b80a89a58e3a8f17cc7
lovesk927 <lovesk927@iabacus.co.kr>
2023-02-16 11:12:33 +0900

    2023-02-15 메인화면 투약,특이사항 기능 구현

--- app/src/main/java/kkkb1114/sampleproject/bodytemperature/fragment/HomeFragment.java
+++ app/src/main/java/kkkb1114/sampleproject/bodytemperature/fragment/HomeFragment.java
@@ -1,13 +1,28 @@
 package kkkb1114.sampleproject.bodytemperature.fragment;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import kkkb1114.sampleproject.bodytemperature.MainActivity;
 import kkkb1114.sampleproject.bodytemperature.R;
 import kkkb1114.sampleproject.bodytemperature.ThermometerView;
 
@@ -15,7 +30,11 @@ public class HomeFragment extends Fragment {
 
     TextView tv_temperature;
     ThermometerView thermometer;
-
+    ImageView pill;
+    ImageView significant;
+    private SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    Context context;
     public HomeFragment() {
 
     }
@@ -23,15 +42,23 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        context = getActivity();
+        preferences = context.getSharedPreferences("timeLineData", MODE_PRIVATE);
+        editor= preferences.edit();
 
         initView(view);
+        this.setListner();
         return view;
     }
 
     public void initView(View view){
         tv_temperature = view.findViewById(R.id.tv_temperature);
         thermometer = view.findViewById(R.id.thermometer);
+        pill = view.findViewById(R.id.iv_pill);
+        significant = view.findViewById(R.id.iv_significant);
     }
 
     public void setTextTvTemperature(String data){
@@ -45,4 +72,62 @@ public class HomeFragment extends Fragment {
             thermometer.setCurValue(data);
         }
     }
+
+    public void setListner()
+    {
+        pill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final LinearLayout linear = (LinearLayout) View.inflate(getActivity(), R.layout.dialog_pill, null);
+                new AlertDialog.Builder(getActivity()).setView(linear)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                EditText edt_pill = (EditText) linear.findViewById(R.id.edt_pill);
+                                String value = edt_pill.getText().toString();
+                                long now =System.currentTimeMillis();
+                                Date date = new Date(now);
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+                                editor.putString(dateFormat.format(date),"pill:"+value);
+                                editor.commit();
+                                dialog.dismiss();
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.dismiss();
+                    }
+                }).show();
+            }
+        });
+
+        significant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final LinearLayout linear = (LinearLayout) View.inflate(getActivity(), R.layout.dialog_significant, null);
+                new AlertDialog.Builder(getActivity()).setView(linear)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                EditText edt_significant = (EditText) linear.findViewById(R.id.edt_significant);
+                                String value = edt_significant.getText().toString();
+                                long now =System.currentTimeMillis();
+                                Date date = new Date(now);
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+                                editor.putString(dateFormat.format(date),"significant:"+value);
+                                editor.commit();
+                                dialog.dismiss();
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                dialog.dismiss();
+                            }
+                        }).show();
+            }
+        });
+    }
+
+
+
 }
