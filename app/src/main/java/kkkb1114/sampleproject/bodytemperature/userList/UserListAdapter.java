246cde58e73b2d5d7aa42b07345334c14e24a35e
kkkb1114 <kikkb1114@naver.com>
2023-02-21 09:32:13 +0900

    2023.02.21 1. 사용자 리스트 삭제 구현 중

--- app/src/main/java/kkkb1114/sampleproject/bodytemperature/userList/UserListAdapter.java
+++ app/src/main/java/kkkb1114/sampleproject/bodytemperature/userList/UserListAdapter.java
@@ -1,7 +1,9 @@
 package kkkb1114.sampleproject.bodytemperature.userList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +28,7 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHo
     public UserListAdapter(Context context, ArrayList<String> userList){
         this.context = context;
         this.userList = userList;
+        PreferenceManager.PREFERENCES_NAME = "user_list";
     }
 
     @NonNull
@@ -40,8 +43,19 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHo
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String name = userList.get(position);
         holder.tv_user_name.setText(name);
+
         setUserNameClick(holder, name);
         setModifyClick(holder);
+        setParentViewLongClick(holder, name);
+        setSelectUser(holder, name);
+    }
+
+    /** 현재 사용자 구분 **/
+    public void setSelectUser(ViewHolder holder, String name){
+        String select_user_name = PreferenceManager.getString(context, "select_user_name");
+        if (select_user_name != null && select_user_name.equals(name)){
+            holder.ln_user.setBackgroundColor(context.getResources().getColor(R.color.user_list_select_user,null));
+        }
     }
 
     /** 사용자 이름 클릭 이벤트 **/
@@ -68,6 +82,49 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHo
             }
         });
     }
+
+    /** 아이템 부모뷰 롱클릭 이벤트 **/
+    public void setParentViewLongClick(ViewHolder holder, String name){
+        holder.ln_user.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+                builder.setTitle("사용자 삭제")
+                        .setMessage("사용자를 삭제하시겠습니까?")
+                        .setPositiveButton("예", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                PreferenceManager.PREFERENCES_NAME = "user_list";
+                                String select_user_name = PreferenceManager.getString(context, "select_user_name");
+
+                                PreferenceManager.PREFERENCES_NAME = name+"Profile";
+                                PreferenceManager.clear(context);
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
+        });
+    }
+
+    /** 현재 선택된 사용자는 삭제 할수 없다는 창 **/
+    /*public void asd(){
+
+        if (name.equals(select_user_name)){
+
+        }else {
+
+        }
+    }*/
 
     @Override
     public int getItemCount() {
